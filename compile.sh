9ee8b29ba046bb762f62c4544dda06cd17888c27
Pelle van Gils <pelle@hwky.ai>
2021-06-25 15:24:39 +0200

    cmake: add cuda/targets include path
    Signed-off-by: Pelle van Gils <pelle@hwky.ai>

--- compile.sh
+++ compile.sh
@@ -1,6 +1,7 @@
 rm -rf load_trt_engine
 g++ load_trt_engine.cpp flattenConcatCustom.cpp -o load_trt_engine\
     -I /usr/include/x86_64-linux-gnu \
+    -I /usr/local/cuda-11.2/targets/x86_64-linux/include \
     -I /usr/local/cuda/include \
     -std=c++11 -ldl -lpthread -lrt\
     -L/usr/lib/x86_64-linux-gnu \
